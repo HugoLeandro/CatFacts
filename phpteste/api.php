46a14ea476175a5d07177572426cbaaf0014a49a
hugoleandro <127011635+HugoLeandro@users.noreply.github.com>
2024-12-19 07:17:26 -0300

    Update api.php

--- phpteste/api.php
+++ phpteste/api.php
@@ -2,17 +2,24 @@
 // URL da API
 $api_url = "https://catfact.ninja/fact";
 
-try {
-    // Faz a requisição à API
-    $response = file_get_contents($api_url);
-    $data = json_decode($response, true);
+// Inicia cURL
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-    // Retorna o fato
-    if (isset($data['fact'])) {
-        echo $data['fact'];
-    } else {
-        echo "Erro ao buscar a API.";
-    }
-} catch (Exception $e) {
-    echo "Erro: " . $e->getMessage();
+$response = curl_exec($ch);
+$error = curl_error($ch);
+curl_close($ch);
+
+if ($response === false) {
+    echo "Erro: não foi possível realizar a requisição. Detalhes: " . $error;
+    exit;
+}
+
+$data = json_decode($response, true);
+if (isset($data['fact'])) {
+    echo $data['fact'];
+} else {
+    echo "Erro ao buscar a API.";
 }
